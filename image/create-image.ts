070c082496130b62c093fe8ea5ab4bceee584864
ofadiman <ofadiman@gmail.com>
2022-11-05 21:08:45 +0100

    refactor: regenerate pepe the frog images

--- image/create-image.ts
+++ image/create-image.ts
@@ -1,16 +1,17 @@
 import { openai } from '../openai.js'
+import { AxiosError } from 'axios'
 
 try {
   const response = await openai.createImage({
-    prompt: 'pepe the frog',
-    n: 1,
+    prompt:
+      'Pepe the Frog, internet meme consisting of a green anthropomorphic frog with a humanoid body.',
+    n: 5,
     size: '1024x1024',
-    response_format: 'url',
   })
 
-  if (response.data.data[0]) {
-    console.log(response.data.data[0])
-  }
+  response.data.data.forEach((value) => {
+    console.log(value.url)
+  })
 } catch (error) {
-  console.error(error)
+  console.error(((error as AxiosError).response?.data as { error: unknown }).error)
 }
